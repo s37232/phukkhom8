2998de4b33ef4055b1d388a6154f93eded881493
s37232 <s37232@banbung.ac.th>
2026-06-12 15:03:38 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
 // เพิ่ม JavaScript สำหรับการโต้ตอบกับผู้ใช้ (Optional)
-document.addEventListener('DOMContentLoaded', function () {
-    console.log('Portfolio ของ เพียงตะวัน พร้อมแล้ว!');
+document.addEventListener('DOMContentLoaded', function () {  
 });
